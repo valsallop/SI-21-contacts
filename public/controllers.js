1b25a3a908fdfbd5968e615063237a3ebbca0761
Valentin Salinas <valentin.sallop@gmail.com>
2015-11-16 18:01:42 +0100

    mysql-dani

--- public/controllers.js
+++ public/controllers.js
@@ -1,27 +1,53 @@
-var myApp = angular.module("ContactListApp",[]);
+var myApp= angular.module("ContactListApp",[]);
 
 myApp.controller('AppCtrl',['$scope','$http',function($scope,$http){
 	console.log("Controller initialized");
-
-	var refresh = function (){
-		$http.get('/contacts').success(function (contacts){
-			console.log('Data received successfully');
-			$scope.contactlist = contacts;
-		});
-	}
-
+	
+	var refresh=function(){
+		/* /contacts */
+		/* http://localhost:57478/Home/Get*/
+	$http.get('/contacts').success(function (contacts){
+		console.log('Data received');
+		$scope.contactlist=contacts;
+	});
+	};
 	refresh();
-
-	$scope.addContact = function(){
-		console.log("Inserting contact ...");
+	$scope.addContact= function(){
+		console.log('inserting contact');
+		/*$http.post('/contacts').success(function (contacts){
+		console.log('Data received');
+		$scope.contactlist=contacts;
+		});*/
 		$http.post('/contacts',$scope.contact);
 		refresh();
-	}
 
-	$scope.deleteContact = function(id){
-		console.log("Deleting contact with "+id);
+
+	}
+	$scope.deleteContact= function(id){
+		console.log('deleting contact with id',id);
+		/*$http.post('/contacts').success(function (contacts){
+		console.log('Data received');
+		$scope.contactlist=contacts;
+		});*/
 		$http.delete('/contacts/'+id);
 		refresh();
+
 	}
 
+/*
+	person1={
+		name:'Paco',
+		email:'Paco@gmail.com',
+		number:'654 321 302'
+	};
+	person2={
+		name:'Luisa',
+		email:'Luisa@gmail.com',
+		number:'642 124 402'
+	};
+
+	var contactlist=[person1,person2]
+
+	$scope.contactlist=contactlist;
+	*/
 }]);
--- server.js
+++ server.js
@@ -1,75 +1,119 @@
 var express = require('express');
 var path = require('path');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var DataStore = require('nedb');
+var fs= require('fs');
+var bodyParser= require('body-parser');
+var DataStore= require('nedb');
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '173.194.80.204',
+  user     : 'root',
+  password : 'root',
+  database : 'contacts'
+});
 
-var app = express();
+connection.connect();
 
-var dbFileName  = path.join(__dirname,'contacts.json');
-var db = new DataStore({
-		filename : dbFileName,
-		autoload: true
-	});
+var app= express();
 
-console.log('DB initialized');
-
-db.find({},function (err,contacts){
-
-	if(contacts.length == 0){
-		console.log('Empty DB, loading initial data');
-
-		person1 = {
-			name : 'Pablo',
-			email : 'pafmon@gmail.com',
-			number: '682 123 123'
-		};
-
-		person2 = {
-			name : 'Pedro',
-			email : 'pedro@gmail.com',
-			number: '682 122 126'
-		};
-
-		db.insert([person1, person2]);
-
-	}else{
-		console.log('DB has '+contacts.length+' contacts ');
-	}
+var dbFileName = path.join(__dirname,'contacts.json');
+var db= new DataStore({
+filename: dbFileName,
+autoload:true
 
 });
 
+console.log('DB initialized');
+db.find({},function (err, contacts){
+	if(contacts.length == 0){
+		console.log('Empty db,loading initial data');
+		person1={
+			name:'Paco',
+			email:'Paco@gmail.com',
+			number:'654 321 302'
+		};
+		person2={
+			name:'Luisa',
+			email:'Luisa@gmail.com',
+			number:'642 124 402'
+		};
+
+		db.insert([person1,person2]);
+	}else{
+		console.log('DB has '+contacts.length+' contacts');
+	}
+});
+
+/*
+app.get("/",function(req,res){
+
+	res.send("Hello world,i'm here!");
+
+});
+*/
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+/*
 app.get('/contacts',function(req,res){
-	console.log('New GET request');
+	console.log("new GET request");
 
 	db.find({},function (err,contacts){
+
 		res.json(contacts);
 	});
+});*/
+app.get("/contacts",function(req,res){
+connection.query('SELECT * from contacts', function(err, rows, fields) {
+
+  if (!err){
+    console.log('The solution is: ', rows);
+	res.json(rows);
+  }
+  else
+    console.log('Error while performing Query.');
+  });
 });
 
 app.post('/contacts',function(req,res){
-	console.log('New POST request');
-	console.log(req.body);
-	db.insert(req.body);
-});
+	console.log("new POST request");
+	console.log(req.body)
 
+	connection.query("insert into contacts (name,email,number) values ('"+req.body.name+"','"+req.body.email+"','"+req.body.number+"')", function(err, rows, fields) {
+
+  	if (!err){
+    	console.log("Insert OK");
+    	console.log(JSON.stringify(req.body.name));
+  	}
+  	else
+    	console.log('Error while performing Query.');
+  	});
+
+
+});
 app.delete('/contacts/:id',function(req,res){
-	var id = req.params.id;
-	console.log('New DELETE request for contact with id '+id);
+	console.log("new DELETE request");
+	var id= req.params.id;
+	console.log(id)
+	connection.query("delete from contacts where id = "+id, function(err, rows, fields) {
+
+  	if (!err){
+    	console.log("Insert OK");
+    	console.log(JSON.stringify(req.body.name));
+  	}
+  	else
+    	console.log('Error while performing Query.');
+  	});
 	
-	db.remove({ _id: id},{}, function(err,numRemoved){
-		console.log("Contacts removed: "+numRemoved);
-		if(numRemoved  == 1)
-			res.sendStatus(200);
-		else
-			res.sendStatus(404);
-	});
+
 });
 
-app.listen(5000);
-console.log('Magic is happening on port 5000');
 
+
+app.listen(8080);
+console.log('Server is on fire');
